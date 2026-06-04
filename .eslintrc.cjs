e44ba61ecc83013be699c1a18d4c9d8b3f4448eb
xanning <119341546+xanning@users.noreply.github.com>
2026-06-04 20:57:05 +0300

    fuck off

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,6 @@ const a11yOff = Object.keys(require("eslint-plugin-jsx-a11y").rules).reduce(
   },
   {},
 );
-
 module.exports = {
   env: {
     browser: true,
@@ -106,12 +105,13 @@ module.exports = {
         allowSeparatedGroups: true,
       },
     ],
-   "prettier/prettier": "off",
+    "prettier/prettier": "off",
     "import/order": "off",
     "sort-imports": "off",
     "prefer-template": "off",
     "react/no-array-index-key": "off",
     "spaced-comment": "off",
- ...a11yOff,
+    "react/no-unescaped-entities": "off", // added
+    ...a11yOff,
   },
 };
