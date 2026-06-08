204391498caee9be14c820e0112d3c932403928d
xanning <sptrspostok@gmail.com>
2026-06-08 22:50:11 +0300

    NO ESLINT I AM NOT GOING TO WRITE GOOD CODE

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -111,14 +111,9 @@ module.exports = {
     "prefer-template": "off",
     "react/no-array-index-key": "off",
     "spaced-comment": "off",
-<<<<<<< deving
     "react/no-unescaped-entities": "off",
     "import/no-unresolved": "off",
     "import/extensions": "off",
- ...a11yOff,
-=======
-    "react/no-unescaped-entities": "off", // added
     ...a11yOff,
->>>>>>> production
   },
 };
